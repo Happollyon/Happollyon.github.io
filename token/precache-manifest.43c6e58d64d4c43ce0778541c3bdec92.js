b49b80f4cd28df05619b7ecc3f1502f9f66eedac
Happollyon <36013973+Happollyon@users.noreply.github.com>
2020-09-29 11:56:27 +0100

    ;;

---
+++ token/precache-manifest.43c6e58d64d4c43ce0778541c3bdec92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413f77cdf34e807e086856123c45acd4",
+    "url": "/twitterToken/index.html"
+  },
+  {
+    "revision": "26d665586152b00d1913",
+    "url": "/twitterToken/static/css/main.72dd9e78.chunk.css"
+  },
+  {
+    "revision": "898fdfee380616ee0341",
+    "url": "/twitterToken/static/js/2.bd4ddd77.chunk.js"
+  },
+  {
+    "revision": "26d665586152b00d1913",
+    "url": "/twitterToken/static/js/main.042ea19f.chunk.js"
+  },
+  {
+    "revision": "92ec081d02ec9cbfdfa5",
+    "url": "/twitterToken/static/js/runtime-main.87a15a61.js"
+  }
+]);
